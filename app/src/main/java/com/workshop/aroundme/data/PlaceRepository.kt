8eb3c8bc3977a51b984ebb3c1898224ba074040d
Reza Moallemi <rm.allmails@gmail.com>
2019-02-19 09:04:21 +0330

    Add mapper for converting place dto to entity

--- app/src/main/java/com/workshop/aroundme/data/PlaceRepository.kt
+++ app/src/main/java/com/workshop/aroundme/data/PlaceRepository.kt
@@ -1,5 +1,6 @@
 package com.workshop.aroundme.data
 
+import com.workshop.aroundme.data.mapper.toPlaceEntity
 import com.workshop.aroundme.data.model.PlaceEntity
 import com.workshop.aroundme.remote.datasource.PlaceDataSource
 
@@ -7,8 +8,8 @@ class PlaceRepository(private val placeDataSource: PlaceDataSource) {
 
     fun getFeaturedPlaces(success: (List<PlaceEntity>?) -> Unit) {
         Thread {
-            val result = placeDataSource.getFeaturedPlaces()?.map {
-                PlaceEntity(it.full_name, it.address, it.like_count, it.images?.getOrNull(0)?.image?.card?.url)
+            val result = placeDataSource.getFeaturedPlaces()?.map { placeDto ->
+                placeDto.toPlaceEntity()
             }
             success(result)
         }.start()
---
+++ app/src/main/java/com/workshop/aroundme/data/mapper/Mappers.kt
@@ -0,0 +1,12 @@
+package com.workshop.aroundme.data.mapper
+
+import com.workshop.aroundme.data.model.PlaceEntity
+import com.workshop.aroundme.remote.model.response.PlaceDto
+
+fun PlaceDto.toPlaceEntity() = PlaceEntity(
+    name = full_name,
+    address = address,
+    likes = like_count,
+    imageUrl = images?.getOrNull(0)?.image?.card?.url
+)
+
